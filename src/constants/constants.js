7a6b6be2f7e4aae9521c3a4ea5a299ab8fa70f39
Zeel <zeelp741@gmail.com>
2022-01-08 02:39:23 -0500

    updated website Jan-06-2022

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,22 +1,32 @@
 export const projects = [
   {
-    title: 'Post-it ',
-    description: "Full Stack Application ",
-      image: '/images/space-image.jpg',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: 'Expense.ly',
+    description: "An expense tracking application designed for students to improve thier financial spending habits",
+      image: '/images/expense.ly.png',
+      tags: ['React', 'Javascript', 'Context API', 'Speechly'],
+    source: 'https://github.com/zeelp741/Expense-Tracker',
+    visit: 'https://expense-tracker-317.netlify.app/',
     id: 0,
+  }, 
+  
+  {
+    title: 'Post-it ',
+    description: "Developed a photo sharing application that allows users to post images and like or comment on other photos",
+      image: '/images/post-it.png',
+      tags: ['Mongo', 'Express', 'React', 'Node'],
+    source: 'https://github.com/zeelp741/Post-It',
+    visit: 'https://github.com/zeelp741/Post-It',
+    id: 1,
   },
 
   {
-    title: 'Test ',
-    description: "Test Description",
-      image: '/images/hogwarts.png',
-      tags: ['SQL', 'NextJS'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 1,
+    title: 'Hand Gesture Control',
+    description: "An application which allows the user to increase/decrease volume, play/pause music and skip songs by using hand gestures.",
+      image: '/images/hand-gesture.png',
+      tags: ['Python', 'OpenCV', 'Mediapipe', 'Numpy'],
+    source: 'https://github.com/zeelp741/Gesture-Control',
+    visit: 'https://github.com/zeelp741/Gesture-Control',
+    id: 2,
   },
 
   
@@ -28,10 +38,30 @@ export const experiences = [
   {
     title: 'Software Engineer at BMO',
     description: 'September 2021 - Present',
-      image: './images/Logo_BMO.jpeg',
-      tags: ['Java', 'Angular'],
+      image: './images/bmo_image.png',
+      tags: ['Java', 'Angular', 'Postman', 'Jira', 'Python'],
     visit: 'https://google.com',
     id: 0
+
+  },
+
+  {
+    title: 'Lead Developer at Virtual Meetup ',
+    description: 'January 2021 - April 2021',
+      tags: ['React.js', 'Next.js', 'GraphQL', 'Graph CMS', 'Tailwind CSS'],
+      image: './images/virtual-meetup.jpg',
+    visit: 'https://google.com',
+    id: 1
+
+  },
+
+  {
+    title: 'Full Stack Developer at Waterloop',
+    description: 'May 2021 - Present',
+      tags: ['React.js', 'Express', 'Typescript', 'Javscript'],
+      image: './images/waterloop-logo.png',
+    visit: 'https://google.com',
+    id: 2
 
   },
 
@@ -41,31 +71,15 @@ export const experiences = [
       tags: ['MongoDB', 'Express.js', 'Node.js', 'React.js'],
       image: './images/Facedrive_logo.jpg',
     visit: 'https://google.com',
-    id: 1
-
-  },
-  ,
-
-  {
-    title: 'Full Stack Developer at Waterloop',
-    description: 'May 2021 - Present',
-      tags: ['MongoDB', 'Express.js', 'Node.js', 'React.js'],
-      image: './images/waterloop-logo.png',
-    visit: 'https://google.com',
-    id: 2
-
-  },
-
-  ,
-
-  {
-    title: 'Lead Developer at Virtual Meetup ',
-    description: 'January 2021 - April 2021',
-      tags: ['MongoDB', 'Express.js', 'Node.js', 'React.js'],
-      image: './images/virtual-meetup.jpg',
-    visit: 'https://google.com',
     id: 3
 
   },
+  ,
+
+
+
+  ,
+
+
 
 ];
